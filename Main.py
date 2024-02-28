d637dab49577e87c292ddc570ce3ae9cbbb7bbc0
Sowntharya15 <sowntharyas15093@gmail.com>
2024-02-28 20:34:30 +0530

    creating classess

--- Main.py
+++ Main.py
@@ -1,2 +1,13 @@
-print("sowntharya")
-print("Sutheshpravin")
+class Employee:
+    def __init__(self,name, age, salary, gender):
+        self.name=name
+        self.age=age
+        self.salary=salary
+        self.gender=gender
+    def employee_details(self):
+        print("name:",self.name)
+        print("age:",self.age)
+        print("salary:",self.salary)
+        print("gender:",self.gender)
+e1=Employee('Riya',22,80000,'female')
+e1.employee_details()
