738313b8a406b2f9565ad4223719174e188aaab5
Sowntharya15 <sowntharyas15093@gmail.com>
2024-03-07 11:53:51 +0530

    Creating a vehicle class

--- Vehicle.py
+++ Vehicle.py
@@ -5,12 +5,18 @@ class Vehicle:
     def show_details(self):
         print("The mileage is",self.mileage)
         print("The cost of the vehicle is",self.cost)
+    
 Audi = Vehicle(2000,2000000)
 Audi.show_details()
-class Scooter(Vehicle):
-    def __init__(self,mileage,cost):
-        super().__init__(mileage,cost)
-        print("This is a Scooty")
-Scooty = Scooter(500,80000)
-Scooty.show_details()
 
+class Scooter(Vehicle):
+    def __init__(self,mileage,cost,brand):
+        super().__init__(mileage,cost)    # over riding init method
+        self.brand = brand 
+    def show_scooter_details(self):
+        print("This is a Scooty")
+        print("The brand is",self.brand)
+     
+Scooty = Scooter(500,80000,"Vespa")
+Scooty.show_details()
+Scooty.show_scooter_details()
